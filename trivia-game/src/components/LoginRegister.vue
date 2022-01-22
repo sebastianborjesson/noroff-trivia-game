ac5873c175a41724a89ead456f422f85f4a499de
Christopher Vestman <christophervestman@gmail.com>
2022-01-22 20:23:15 +0100

    feat: Added base views and tailwind

---
+++ trivia-game/src/components/LoginRegister.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import { ref } from "vue";
+    import { useStore } from "vuex"
+
+    // const store = 
+
+</script>
+
+<template>
+    <h1>Hej</h1>
+</template>
---
+++ trivia-game/src/views/Start.vue
@@ -0,0 +1,8 @@
+<script setup>
+import LoginRegister from '../components/LoginRegister.vue';
+
+</script>
+
+<template>
+    <LoginRegister />
+</template>
