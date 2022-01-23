d7cc85e709a5f4d173ef5e5090f59ccd674e0a67
sebastianborjesson <sebastian.borjesson@live.se>
2022-01-23 19:25:30 +0100

    feat(Question): Built the framework for question view

--- trivia-game/src/views/Question.vue
+++ trivia-game/src/views/Question.vue
@@ -1,7 +1,10 @@
 <script setup>
+import DisplayQuestion from '../components/DisplayQuestion.vue';
 
 </script>
 
 <template>
-
+    <main class="container mx-auto px-4">
+        <DisplayQuestion />
+    </main>
 </template>
