4ea243979d05268dd1d8e4647e05c8516d78a363
Christopher Vestman <christophervestman@gmail.com>
2022-01-23 17:58:24 +0100

    FEAT: add initUser func

--- trivia-game/src/store.js
+++ trivia-game/src/store.js
@@ -1,14 +1,32 @@
 import { createStore } from "vuex";
 
+const initUser = () => {
+    const storedUser = localStorage.getItem("trivia-user")
+    if (!storedUser) {
+        return null
+    }
+    return JSON.parse(storedUser)
+}
+
 
 export default createStore({
     state: {
-        
+        user: initUser(), // Well username...
+        diffuculty: null,
+        numberOfQuestion: null,
+        categories: null,                        
+    },
+    getters: {
+        // filterQuestionsByCategoryID:
     },
     mutations: {
-        
+        setUser: (state, user) => {
+            state.user = user
+        },
+        // setDifficulty?
+        // setCategory?
     },
     actions: {
-        
+
     }
 })
