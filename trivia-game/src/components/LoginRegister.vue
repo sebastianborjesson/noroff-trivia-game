d58804af7e5c4df99bbfc6015edde9814224de99
Christopher Vestman <christophervestman@gmail.com>
2022-01-23 13:51:59 +0100

    feat: testing layouts

--- trivia-game/src/components/LoginRegister.vue
+++ trivia-game/src/components/LoginRegister.vue
@@ -2,24 +2,69 @@
     import { ref } from "vue";
     import { useStore } from "vuex"
 
-    // const store = 
+    const store = useStore()
+
+    const username = ref("")
+
+    const onRegisterLoginClick = async () => {
+        const error = await  store.dispatch("loginRegister", {
+            username
+        })
+
+        if (error !== null) {
+            displayError.value = error;
+        } else {
+            emit("onAuthSuccess")
+        }
+    } 
 
 </script>
 
 <template>
-    <div class="border-solid 1px">
-        <h1 class="font-bold text-4xl">123 Fanta och rosé</h1>
-        <input 
-            type="text"
-            placeholder="Username"
-        />
+    <h1 class="font-bold text-4xl">123 Fanta och rosé</h1>
+
+    <div class="container flex items-center justify-between">
+        <form class="mb-3">
+            <fieldset class="mb-3">
+                <label for="username" aria-label="Username" class="block">Username:</label>
+                <input 
+                    type="text"
+                    id="username"
+                    placeholder="Username"
+                    class="border border-slate-300"
+                />
+            </fieldset>
+        </form>
+
+        <form>
+            <fieldset class="mb-3">
+                <h1> Guess populate from API?</h1>
+                <p class="font-bold text-red-600">Only show Difficulty if username is valid</p>
+                <input type="radio" id="any_difficulty" name="select_difficulty" value="random">
+                <label for="any_difficulty">Random</label><br>
+
+                <input type="radio" id="any_difficulty" name="select_difficulty" value="easy">
+                <label for="any_difficulty">Easy</label><br>
+                
+                <input type="radio" id="any_difficulty" name="select_difficulty" value="medium">
+                <label for="any_difficulty">Medium</label><br>
+                
+                <input type="radio" id="any_difficulty" name="select_difficulty" value="hard">
+                <label for="any_difficulty">Hard</label>
+            </fieldset>
+        </form>
+
+        <form>
+            <fieldset class="mb-3">
+                <label for="nrOfQuestions" aria-label="nrOfQuestions" class="block">Number of Questions?</label>
+                <input 
+                    type="number" 
+                    id="nrOfQuestions"
+                    placeholder="10"
+                    class="border border-slate-300"
+                />
+            </fieldset>
+        </form>
     </div>
 
-    <div class="text-red-700">
-        Select Difficulty, Maybe 3 radio btns?
-    </div>
-    <div>
-
-    </div>
-    <p></p>
 </template>
