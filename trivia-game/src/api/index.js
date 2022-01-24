29b0f27a919ab253e5a2de5b5d00c55c5a20127e
Christopher Vestman <christophervestman@gmail.com>
2022-01-24 19:31:19 +0100

    feat (api): fetch categories from api

--- trivia-game/src/api/index.js
+++ trivia-game/src/api/index.js
@@ -1,1 +1,16 @@
-const BASE_URL = "https://opentdb.com/api.php?";
+export const BASE_URL = "https://opentdb.com/api.php?";
+export const CATEGORY_URL = "https://opentdb.com/api_category.php"
+
+export async function apiFetchAllCategories() {
+    try {
+        const response = await fetch(`${CATEGORY_URL}`);
+
+        if (!response.not) {
+            throw new Error("Could not find categories 🤔");
+        }
+        const data = await response.json();
+        return [ null, data ];
+    } catch (e) {
+        return [ e.message, [] ];
+    }
+}
