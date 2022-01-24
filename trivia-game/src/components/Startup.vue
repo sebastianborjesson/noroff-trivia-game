9662a3cd92d8381084a10da4eccf6307d0ffcf09
Christopher Vestman <christophervestman@gmail.com>
2022-01-24 14:19:03 +0100

    fix (restructuring): restructuring code WIP

--- trivia-game/src/components/Startup.vue
+++ trivia-game/src/components/Startup.vue
@@ -5,41 +5,55 @@
     const store = useStore()
 
     const username = ref("")
-    const categories = computed(() => store.state.categories)
-    const selectedCategory = ref("any")
+    // const categories = computed(() => store.state.categories)
+    const selectedCategory = ref("")
+    const trivia_difficulty = ref("")
+    const trivia_amount = ref("")
 
 
     const storedCategories = ref([])
     store.subscribe((mutation,state)=>{
         storedCategories.value = state.categories;
-        console.log(storedCategories.value);
     })
 
     function selectedCategoryInList() {
-        console.log(selectedCategory.value)
+        // console.log(selectedCategory.value)
         // Want to use this value to set the category ID
+        return selectedCategory.value
     }
 
 
     function onStart() {
-
+        console.log(username.value, trivia_amount.value, selectedCategory.value, trivia_difficulty.value)
+        // user = {
+        //     username: 
+        // }
     }
     
 </script>
 
 <template>
-    <main>
-        <h1 class="font-bold text-4xl">123 Fanta och rosé</h1>
+    <header class="mb-2">
+        <h1 class="font-bold text-4xl flex justify-center pt-5">Trivia Quiz</h1>
+    </header>
 
-        <form @submit.prevent="onSubmit" class="flex items-center justify-between">
-            <fieldset>
+    <div class="container flex justify-evenly">
+        <form class="ml-3">
+            <fieldset class="mb-3">
                 <label for="username" aria-label="Username" class="block">Username</label>
-                <input type="text" id="username" v-model="username" />
+                <input type="text" id="trivia_username" v-model="username" class="border border-slate-300"/>
             </fieldset>
+        </form>
+        <!-- Check if Username is available -->
+        <button type="submit" class="bg-indigo-500 text-white p-3 rounded">Register</button>
+    </div>
+
+    <div class="container">
+        <form @submit.prevent="onSubmit" class="">        
 
             <fieldset>
                 <label for="trivia_amount">Number of Questions:</label>
-                <input type="number" name="trivia_amount" id="trivia_amount">
+                <input type="number" name="trivia_amount" id="trivia_amount" v-model="trivia_amount">
             </fieldset>
             
             <fieldset>
@@ -53,17 +67,19 @@
 
             <fieldset>
                 <label for="difficulty">Select Difficulty: </label>
-                <select name="trivia_difficulty" class="form-control">
+                <select name="trivia_difficulty" class="form-control" v-model="trivia_difficulty">
                     <option value="any">Any Difficulty</option>
                     <option value="easy">Easy</option>
                     <option value="medium">Medium</option>
                     <option value="hard">Hard</option>
                 </select>
             </fieldset>
-            
-            <button type="submit" @click="onStart">STARTUURUUU</button>
         </form>
-    </main>
+    </div>
+        
+    <div>
+        <button type="submit" @click="onStart" class="bg-indigo-500 text-white p-3 rounded">STARTUURUUU</button>
+    </div>
 </template>
 
 <style scoped></style>
