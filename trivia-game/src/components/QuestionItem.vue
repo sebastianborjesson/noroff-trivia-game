e813da5375ed6e8be7503e121936d04b671e3441
sebastianborjesson <sebastian.borjesson@live.se>
2022-01-25 21:13:14 +0100

    feat: change question when answering question + route change when all questions answered

--- trivia-game/src/components/QuestionItem.vue
+++ trivia-game/src/components/QuestionItem.vue
@@ -1,58 +1,41 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 import { useStore } from 'vuex';
+import { useRouter } from 'vue-router'
 
 const store = useStore()
+const router = useRouter();
+const currentQuestion = computed (() => store.getters.getCurrentQuestion)
+const index = computed (() => currentQuestion.value.questionNumber)
+const questionAmount = computed(() => store.state.questions)
 
 
-let currentQuestion = store.getters.currentQuestion;
-    console.log(currentQuestion);
-const questions = computed(() => store.getters.getQuestions)
-
-const storedQuestions = ref([])
-store.subscribe((mutation, state) => {
-    storedQuestions.value = store.getters.getQuestions;
-})
-
-function answerQuestion () {
-    currentQuestion += 1;
-    console.log(storedQuestions);
-    console.log(currentQuestion);
-
+function answerQuestion() {
+    if(index.value >= questionAmount.value.length - 1) {
+        router.push("/result");
+    } else {
+        store.commit("setCurrentQuestion", [index.value + 1])
+    }
 }
-console.log(storedQuestions);
-
 
 </script>
 <template>
-<div id="app" class="flex w-full h-screen justify-center items-center">
-        <div v-if="!storedQuestions[currentQuestion]">
-            <p>loading</p>
-
-        </div>
+    <div id="app" class="flex w-full h-screen justify-center items-center">
         
-        <div v-if="storedQuestions[currentQuestion]" :key="currentQuestion" class="w-full max-w-xl p-3">
-            <h1 class="font-bold text-5xl text-center text-indigo-700">
-                Question: {{ currentQuestion + 1 }}
-            </h1>
+        <div class="w-full max-w-xl p-3">
+            <h1 class="font-bold text-5xl text-center text-indigo-700">Question: {{ index + 1 }}</h1>
             <div class="bg-white p-12 rounded-lg shadow-lg w-full mt-8">
-                
-                <p class="text-2xl font-bold">
-                    {{storedQuestions[currentQuestion].question}}
-                </p>
-                <button v-for="(answers, index) in storedQuestions[currentQuestion].answers" 
-                :key="answers" 
-                :value="index" 
-                type="button" 
-                class="block mt-4 border w-full text-left 
-                border-gray-300 rounded-lg py-2 px-6 text-lg"
-                @click="answerQuestion">
-                {{answers}}
-                </button>
+                <p class="text-2xl font-bold">{{ currentQuestion.question }}</p>
+                <button
+                    v-for="(answer, index) in currentQuestion.answers" :key="index"
+                    
+                    type="button"
+                    class="block mt-4 border w-full text-left border-gray-300 rounded-lg py-2 px-6 text-lg"
+                    @click="answerQuestion"
+                >{{ answer }}</button>
             </div>
         </div>
     </div>
 </template>
 <style>
-
 </style>
